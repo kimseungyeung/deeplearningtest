c2f653c9dd89c5a025ea7eb2ca675710844baf2c
Kimseungyeung <seungyeung1@naver.com>
2019-04-24 18:11:42 +0900

    차트추가 및 레이아웃정리1

--- app/src/main/java/com/example/myapplication/Data/NoPayData.java
+++ app/src/main/java/com/example/myapplication/Data/NoPayData.java
@@ -1,6 +1,9 @@
-package com.example.myapplication;
+package com.example.myapplication.Data;
 
-public class NoPayData {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class NoPayData implements Parcelable {
     private int statenum=0;
    private int statetype =0;
    private String statename="";
@@ -18,6 +21,44 @@ public class NoPayData {
         this.rewardman=rm;
         this.researchman=rsm;
         this.checkview=check;
+    }
+    public NoPayData(Parcel parcel) {
+        // must be same order with writeToParcel()
+        this.statenum = parcel.readInt();
+        this.statetype = parcel.readInt();
+        this.customername = parcel.readString();
+        this.statename = parcel.readString();
+        this.statetext = parcel.readString();
+        this.rewardman = parcel.readString();
+        this.researchman = parcel.readString();
+        this.checkview= (parcel.readInt()==0)? false : true;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.statenum);
+        dest.writeInt(this.statetype);
+        dest.writeString(this.customername);
+        dest.writeString(this.statename);
+        dest.writeString(this.statetext);
+        dest.writeString(this.rewardman);
+        dest.writeString(this.researchman);
+        dest.writeInt(this.checkview?1:0);
+    }
+    // create Parcelable
+    public static final Parcelable.Creator<NoPayData> CREATOR = new Parcelable.Creator<NoPayData>() {
+        @Override
+        public NoPayData createFromParcel(Parcel parcel) {
+            return new NoPayData(parcel);
+        }
+        @Override
+        public NoPayData[] newArray(int size) {
+            return new NoPayData[size];
+        }
+    };
+    @Override
+    public int describeContents() {
+        return 0;
     }
 
     public int getStatenum() {
